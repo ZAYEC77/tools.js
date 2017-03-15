8c5b683113736ff9fdbfa98726de1a3c79603aa1
Dmytro Karpovych <ZAYEC77@gmail.com>
2017-03-15 14:40:24 +0200

    add cookie

--- tools.js
+++ tools.js
@@ -85,5 +85,33 @@ var tools = {
             return false;
         }
         return true;
+    },
+    cookie: {
+        set: function (key, value, days) {
+            var expires = "";
+            if (days) {
+                var date = new Date();
+                date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+                expires = "; expires=" + date.toUTCString();
+            }
+            document.cookie = key + "=" + value + expires + "; path=/";
+        },
+
+        get: function (key) {
+            var keyEq = key + "=";
+            var ca = document.cookie.split(';');
+            for (var i = 0; i < ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0) == ' ') {
+                    c = c.substring(1, c.length);
+                }
+                if (c.indexOf(keyEq) == 0) return c.substring(keyEq.length, c.length);
+            }
+            return null;
+        },
+
+        delete: function (key) {
+            tools.cookie.set(key, "", -1);
+        }
     }
 };
